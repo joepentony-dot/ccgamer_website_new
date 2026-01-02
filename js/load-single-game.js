06f876b1d153c5b3c9d325747f05a3dc1aa00c73
joepentony-dot <joepentony@gmail.com>
2026-01-02 14:56:15 +0000

    Add legacy slug fallback map

--- js/load-single-game.js
+++ js/load-single-game.js
@@ -16,6 +16,137 @@ let CCG_SINGLE_ALL_GAMES = [];
 let CCG_SCREENSHOTS = [];
 let CCG_SCREENSHOT_INDEX = 0;
 
+// Legacy slug fallback (SEO preservation)
+const LEGACY_SLUG_MAP = {
+    "10-out": "x-out",
+    "5-the-computer-game": "v:_the_computer_game",
+    "alcazar_the_forgotten_fortress": "alcazar:_the_forgotten_fortress",
+    "aliens_the_computer_game": "aliens:_the_computer_game",
+    "apollo_18_mission_to_the_moon": "apollo_18:_mission_to_the_moon",
+    "archon_the_light_and_the_dark": "archon:_the_light_and_the_dark",
+    "barbarian-2-the-dungeon-of-drax": "barbarian_ii:_the_dungeon_of_drax",
+    "barbarian_ii_the_dungeon_of_drax": "barbarian_ii:_the_dungeon_of_drax",
+    "barbarian_the_ultimate_warrior": "barbarian:_the_ultimate_warrior",
+    "batman_the_caped_crusader": "batman:_the_caped_crusader",
+    "batman_the_movie": "batman:_the_movie",
+    "bc-2-grogs-revenge": "b.c._ii:_grog's_revenge",
+    "bc_bill": "b.c._bill",
+    "bc_ii_grogs_revenge": "b.c._ii:_grog's_revenge",
+    "bcs_quest_for_tires": "b.c.'s_quest_for_tires",
+    "beach-head-2": "beach-head_ii",
+    "beach_head": "beach-head",
+    "beach_head_ii": "beach-head_ii",
+    "blood_n_guts": "blood_'n_guts",
+    "bozos_night_out": "bozo's_night_out",
+    "bullys_sporting_darts": "bully's_sporting_darts",
+    "cauldron-2-the-pumpkin-strikes-back": "cauldron_ii:_the_pumpkin_strikes_back",
+    "cauldron_ii_the_pumpkin_strikes_back": "cauldron_ii:_the_pumpkin_strikes_back",
+    "caveman_ugh_lympics": "caveman_ugh-lympics",
+    "chase_hq": "chase_h.q.",
+    "cops_n_robbers": "cops_'n'_robbers",
+    "creatures-ii-torture-trouble": "creatures_2:_torture_trouble",
+    "creatures_2_torture_trouble": "creatures_2:_torture_trouble",
+    "d_generation": "d/generation",
+    "daley_thompsons_decathlon": "daley_thompson's_decathlon",
+    "dan-dare-2-mekons-revenge": "dan_dare_ii:_mekon's_revenge",
+    "dan_dare_ii_mekons_revenge": "dan_dare_ii:_mekon's_revenge",
+    "dan_dare_pilot_of_the_future": "dan_dare:_pilot_of_the_future",
+    "davy_king_of_the_wild_frontier": "davy:_king_of_the_wild_frontier",
+    "double-dragon-2-the-revenge": "double_dragon_ii:_the_revenge",
+    "double_dragon_ii_the_revenge": "double_dragon_ii:_the_revenge",
+    "dragons-lair-2-escape-from-singes-castle": "dragon's_lair_ii_:_escape_from_singe's_castle",
+    "dragons_lair": "dragon's_lair",
+    "dragons_lair_ii_escape_from_singes_castle": "dragon's_lair_ii_:_escape_from_singe's_castle",
+    "ducktales_the_quest_for_gold": "ducktales:_the_quest_for_gold",
+    "dune-2-the-battle-for-arrakis": "dune_ii:_the_battle_for_arrakis",
+    "dune_ii_the_battle_for_arrakis": "dune_ii:_the_battle_for_arrakis",
+    "eve-of-the-beholder-2-the-legend-of-darkmoon": "eve_of_the_beholder_ii:_the_legend_of_darkmoon",
+    "eve_of_the_beholder_ii_the_legend_of_darkmoon": "eve_of_the_beholder_ii:_the_legend_of_darkmoon",
+    "everyones_a_wally": "everyone's_a_wally",
+    "face_off": "face_off!",
+    "fiendish_freddys_big_top_o_fun": "fiendish_freddy's_big_top_o'_fun",
+    "fist-2-the-legend-continues": "fist_ii:_the_legend_continues",
+    "fist_ii_the_legend_continues": "fist_ii:_the_legend_continues",
+    "flashback_the_quest_for_identity": "flashback:_the_quest_for_identity",
+    "flimbos_quest": "flimbo's_quest",
+    "gary_linekers_superstar_soccer": "gary_lineker's_superstar_soccer",
+    "ghosts_n_goblins": "ghosts_'n_goblins",
+    "gribblys_day_out": "gribbly's_day_out",
+    "he_man_and_the_masters_of_the_universe_the_ilearth_stone": "he-man_and_the_masters_of_the_universe:_the_ilearth_stone",
+    "he_man_and_the_masters_of_the_universe_the_movie": "he-man_and_the_masters_of_the_universe:_the_movie",
+    "henrys_house": "henry's_house",
+    "herberts_dummy_run": "herbert's_dummy_run",
+    "ik_amiga": "ik+_amiga",
+    "ik_c64": "ik+_c64",
+    "indiana_jones_and_the_last_crusade_adventure": "indiana_jones_and_the_last_crusade_(adventure)",
+    "indiana_jones_and_the_last_crusade_the_action_game": "indiana_jones_and_the_last_crusade:_the_action_game",
+    "into_the_eagles_nest": "into_the_eagle's_nest",
+    "invade_a_load": "invade-a-load",
+    "ivan_ironman_stewarts_super_off_road": "ivan_'ironman'_stewart's_super_off_road",
+    "jack-the-nipper-2-in-coconut-capers": "jack_the_nipper_ii:_in_coconut_capers",
+    "jack_the_nipper_ii_in_coconut_capers": "jack_the_nipper_ii:_in_coconut_capers",
+    "james_pond_underwater_agent": "james_pond:_underwater_agent",
+    "jimmy_whites_whirlwind_snooker": "jimmy_white's_whirlwind_snooker",
+    "kane-kane-ii": "kane_&_kane_2",
+    "kane_kane_2": "kane_&_kane_2",
+    "kings_bounty": "king's_bounty",
+    "kung_fu_master": "kung-fu_master",
+    "last-ninja-ii-back-with-a-vengeance": "last_ninja_2:_back_with_a_vengeance",
+    "last_ninja_2_back_with_a_vengeance": "last_ninja_2:_back_with_a_vengeance",
+    "marias_christmas_box": "maria's_christmas_box",
+    "montezumas_revenge": "montezuma's_revenge",
+    "moonstone_a_hard_days_knight": "moonstone:_a_hard_days_knight",
+    "mountie_micks_death_ride": "mountie_mick's_death_ride",
+    "myth_history_in_the_making": "myth:_history_in_the_making",
+    "nightbreed_the_action_game": "nightbreed:_the_action_game",
+    "north_south": "north_&_south",
+    "pit_fighter": "pit-fighter",
+    "pitfall-2-lost-caverns": "pitfall_ii:_lost_caverns",
+    "pitfall_ii_lost_caverns": "pitfall_ii:_lost_caverns",
+    "project-10": "project-x",
+    "project_x": "project-x",
+    "push_over": "push-over",
+    "rambo-first-blood-part-2": "rambo:_first_blood_part_ii",
+    "rambo_first_blood_part_ii": "rambo:_first_blood_part_ii",
+    "rescue_on_fractalus": "rescue_on_fractalus!",
+    "rockn_wrestle": "rock'n_wrestle",
+    "saboteur-2-avenging-angel": "saboteur_ii:_avenging_angel",
+    "saboteur_ii_avenging_angel": "saboteur_ii:_avenging_angel",
+    "samurai_warrior_the_battles_of_usagi_yojimbo": "samurai_warrior:_the_battles_of_usagi_yojimbo",
+    "santas_xmas_caper": "santa's_xmas_caper",
+    "seek_destroy": "seek_&_destroy",
+    "shao_lins_road": "shao-lin's_road",
+    "shoot_out": "shoot-out",
+    "sid_meiers_pirates": "sid_meier's_pirates!",
+    "skull_crossbones": "skull_&_crossbones",
+    "smash_tv": "smash_t.v.",
+    "spartacus_the_swordslayer": "spartacus:_the_swordslayer",
+    "speedball-ii-brutal-deluxe": "speedball_2:_brutal_deluxe",
+    "speedball_2_brutal_deluxe": "speedball_2:_brutal_deluxe",
+    "spy-vs-spy-2-the-island-caper": "spy_vs_spy_ii:_the_island_caper",
+    "spy-vs-spy-3-arctic-antics": "spy_vs_spy_iii:_arctic_antics",
+    "spy_vs_spy_ii_the_island_caper": "spy_vs_spy_ii:_the_island_caper",
+    "spy_vs_spy_iii_arctic_antics": "spy_vs_spy_iii:_arctic_antics",
+    "star_wars_droids": "star_wars:_droids",
+    "strip_poker_a_sizzling_game_of_chance": "strip_poker:_a_sizzling_game_of_chance",
+    "target_renegade": "target:_renegade",
+    "teenage_mutant_hero_turtles_the_coin_op": "teenage_mutant_hero_turtles:_the_coin-op",
+    "terminator-ii-judgment-day": "terminator_2:_judgment_day",
+    "terminator_2_judgment_day": "terminator_2:_judgment_day",
+    "the_castles_of_dr_creep": "the_castles_of_dr._creep",
+    "the_dolphins_rune": "the_dolphin's_rune",
+    "the_train_escape_to_normandy": "the_train:_escape_to_normandy",
+    "track_field": "track_&_field",
+    "turrican-2-the-final-fight": "turrican_ii:_the_final_fight",
+    "turrican_ii_the_final_fight": "turrican_ii:_the_final_fight",
+    "ultima-1-the-first-age-of-darkness": "ultima_i:_the_first_age_of_darkness",
+    "ultima_i_the_first_age_of_darkness": "ultima_i:_the_first_age_of_darkness",
+    "upn_down": "up'n_down",
+    "v_the_computer_game": "v:_the_computer_game",
+    "x_out": "x-out",
+    "yie_ar_kung_fu": "yie_ar_kung-fu"
+};
+
 /* ============================================================
    INIT
 ============================================================ */
@@ -43,6 +174,24 @@ document.addEventListener("DOMContentLoaded", async () => {
             game = CCG_SINGLE_ALL_GAMES.find(
                 g => String(g.id) === gameId
             );
+        }
+
+        if (!game && slugForMatch) {
+            game = CCG_SINGLE_ALL_GAMES.find(
+                g => resolveGameSlug(g.id) === slugForMatch
+            );
+            if (game) gameId = String(game.id);
+        }
+
+        if (!game && slugForMatch) {
+            // Legacy slug fallback (SEO preservation)
+            const legacyId = LEGACY_SLUG_MAP[slugForMatch];
+            if (legacyId) {
+                game = CCG_SINGLE_ALL_GAMES.find(
+                    g => String(g.id) === legacyId
+                );
+            }
+            if (game) gameId = String(game.id);
         }
 
         if (!game && slugForMatch) {
