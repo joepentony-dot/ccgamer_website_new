28e254f57b3f070bf3e1424c4ce477688baaab39
joepentony-dot <joepentony@gmail.com>
2026-01-02 07:43:38 +0000

    Enhance Pacman easter egg

--- js/ccg-global.js
+++ js/ccg-global.js
@@ -263,7 +263,22 @@
             <div class="ccg-pacman__sprite"></div>
             <div class="ccg-pacman__ghost"></div>
         `;
-        setTimeout(() => pacman.remove(), 13000);
+        let chompInterval = null;
+        const startChomp = () => {
+            if (chompInterval) return;
+            let high = true;
+            chompInterval = setInterval(() => {
+                playTone(high ? 880 : 660, "square", 0.08, 0.12);
+                high = !high;
+            }, 180);
+        };
+        startChomp();
+        setTimeout(() => {
+            if (chompInterval) {
+                clearInterval(chompInterval);
+            }
+            pacman.remove();
+        }, 13000);
     }
 
     function triggerBoing() {
@@ -383,6 +398,7 @@
     function triggerCheat(code) {
         const normalized = normalizeCode(code);
         if (cheats[normalized]) {
+            closeSecretModal();
             cheats[normalized]();
         }
     }
