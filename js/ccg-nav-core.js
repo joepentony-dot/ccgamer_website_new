21a60763975ca1a9007d48ec43fadd6109987a33
joepentony-dot <joepentony@gmail.com>
2026-08-05 17:38:39 +0100

    Load Member Hub account sync through the shared module system

--- js/ccg-nav-core.js
+++ js/ccg-nav-core.js
@@ -23,6 +23,7 @@ Do Not Override
         { src: "/js/ccg-amiga-identity.js", marker: "data-ccg-amiga-identity-loader" },
         { src: "/js/ccg-recent-content.js", marker: "data-ccg-recent-content-loader" },
         { src: "/js/ccg-personal-library-controls.js", marker: "data-ccg-personal-library-loader" },
+        { src: "/js/ccg-member-library-sync-loader.js", marker: "data-ccg-member-library-sync-loader" },
         { src: "/js/zzap64-awards-logo-styles.js", marker: "data-ccg-zzap-logo-styles-loader" },
         { src: "/js/ccg-game-badges.js", marker: "data-ccg-game-badges-loader" }
     ];
