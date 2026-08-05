27d85037b43291bc00fd82564b932353c7a63a08
joepentony-dot <joepentony@gmail.com>
2026-08-05 04:58:16 +0100

    Add a maintained Recently Added and Updated strip
    Adds a compact explicit update feed on Home without relying on rebuild timestamps.

--- js/ccg-nav-core.js
+++ js/ccg-nav-core.js
@@ -18,7 +18,8 @@ Do Not Override
         { src: "/js/ccg-archive-shortcuts.js", marker: "data-ccg-archive-shortcuts-loader" },
         { src: "/js/ccg-platform-compare-link.js", marker: "data-ccg-platform-compare-loader" },
         { src: "/js/ccg-publisher-history.js", marker: "data-ccg-publisher-history-loader" },
-        { src: "/js/ccg-amiga-identity.js", marker: "data-ccg-amiga-identity-loader" }
+        { src: "/js/ccg-amiga-identity.js", marker: "data-ccg-amiga-identity-loader" },
+        { src: "/js/ccg-recent-content.js", marker: "data-ccg-recent-content-loader" }
     ];
 
     function isNavPillCandidate(el) {
