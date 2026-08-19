0f11a5b8b41e0c56b2bcaba5cbed321fc8696494
joepentony-dot <joepentony@gmail.com>
2026-08-19 08:39:17 +0100

    Track all shared navigation assets in release fingerprint

--- js/ccg-release-check.js
+++ js/ccg-release-check.js
@@ -20,7 +20,9 @@
     const RELEASE_ASSETS = [
         "/service-worker.js",
         "/js/ccg-nav-core.js",
+        "/js/ccg-nav.js",
         "/js/ccg-nav-fit.js",
+        "/js/ccg-pwa-visible-install.js",
         "/js/ccg-release-check.js",
         "/resources/css/ccg-nav.css",
         "/resources/css/ccg-socials.css",
