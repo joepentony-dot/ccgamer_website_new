c3a8812a5130be9625a05300038dc6ce734f5d99
joepentony-dot <joepentony@gmail.com>
2026-08-19 14:03:22 +0100

    Fingerprint mode identity hit-layer repair

--- js/ccg-release-check.js
+++ js/ccg-release-check.js
@@ -29,6 +29,7 @@
         "/js/ccg-release-check.js",
         "/resources/css/ccg-nav.css",
         "/resources/css/ccg-nav-fit.css",
+        "/resources/css/ccg-mode-identity.css",
         "/resources/css/ccg-socials.css",
         "/resources/css/ccg-community.css",
         "/resources/css/ccg-master.css",
