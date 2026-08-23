5ea1ff2af380ce5e1676576e8ca642b640fd972e
joepentony-dot <joepentony@gmail.com>
2026-08-23 01:55:20 +0100

    Bust cache for refined Lost Sizzler insights UI

--- games/ccg-games/cheeky-commodore-quest/js/asset-overrides.js
+++ games/ccg-games/cheeky-commodore-quest/js/asset-overrides.js
@@ -22,7 +22,7 @@ window.CCG_ASSET_OVERRIDES={
 const CCG_V106_HUD_REV="20260822e";
 const CCG_V106_SIDEBAR_REV="20260822a";
 const CCG_PLAYLIST_AUDIO_REV="20260823a";
-const CCG_PLAYER_INSIGHTS_REV="20260823a";
+const CCG_PLAYER_INSIGHTS_REV="20260823b";
 
 (()=>{
   if(!document.querySelector('link[data-ccg-v106-ui="true"]')){
