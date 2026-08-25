65a775813d0c9e7d4d80e5772ba8a6396f6cb992
joepentony-dot <joepentony@gmail.com>
2026-08-25 06:12:17 +0100

    Keep Lost Sizzler static assets network-first

--- service-worker.js
+++ service-worker.js
@@ -24,6 +24,7 @@ const PUBLIC_SHELL = Object.freeze([
   "/resources/css/ccg-app-launch.css",
   "/resources/css/ccg-global.css",
   "/resources/css/ccg-nav.css",
+  "/resources/css/ccg-nav-core.css",
   "/resources/css/ccg-nav-fit.css",
   "/resources/css/ccg-responsive-safety.css",
   "/resources/css/ccg-responsive-page-polish.css",
@@ -275,7 +276,7 @@ self.addEventListener("fetch", (event) => {
   }
 
   if (STATIC_ASSET_PATTERN.test(url.pathname)) {
-    event.respondWith(cacheFirstAsset(request));
+    event.respondWith(isLostSizzlerPath(url.pathname) ? networkFirstAsset(request) : cacheFirstAsset(request));
   }
 });
 
