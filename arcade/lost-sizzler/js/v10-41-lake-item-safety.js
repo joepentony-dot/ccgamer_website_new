38fb7f14022a219bdd3de09c83b924f766272323
joepentony-dot <joepentony@gmail.com>
2026-08-28 18:31:17 +0100

    Load multimode performance layer after runtime finalizers

--- arcade/lost-sizzler/js/v10-41-lake-item-safety.js
+++ arcade/lost-sizzler/js/v10-41-lake-item-safety.js
@@ -81,4 +81,5 @@
   load("js/v10-41-tutorial-action-finalizer.js","data-ccg-v141-tutorial-action-finalizer");
   load("js/v10-41-r24-live-regressions.js","data-ccg-v141-r24-live-regressions");
   load("js/v10-41-r29-loop-finalizer.js","data-ccg-v141-r29-loop-finalizer");
+  load("js/v10-41-multimode-performance.js","data-ccg-v141-multimode-performance");
 })();
