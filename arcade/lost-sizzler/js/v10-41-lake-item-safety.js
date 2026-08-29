f75bcfc059e8793dcce38343352461e21e46604e
joepentony-dot <joepentony@gmail.com>
2026-08-29 02:05:26 +0100

    Load Colyseus Horde adapter after performance finalizers

--- arcade/lost-sizzler/js/v10-41-lake-item-safety.js
+++ arcade/lost-sizzler/js/v10-41-lake-item-safety.js
@@ -83,4 +83,5 @@
   load("js/v10-41-r29-loop-finalizer.js","data-ccg-v141-r29-loop-finalizer");
   load("js/v10-41-multimode-performance.js","data-ccg-v141-multimode-performance");
   load("js/v10-41-r37-global-performance.js","data-ccg-v141-r37-global-performance");
+  load("js/v10-41-r38-colyseus-horde.js","data-ccg-v141-r38-colyseus-horde");
 })();
