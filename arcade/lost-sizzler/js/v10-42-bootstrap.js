18d714283ea47be510d2add590687163caca1a3d
joepentony-dot <joepentony@gmail.com>
2026-09-07 17:20:20 +0100

    Bump cache for persistent Warden threat guidance

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -5,7 +5,7 @@
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
   const BUILD="V10.42 r3";
-  const CACHE="20260907r12";
+  const CACHE="20260907r13";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-five-depth-campaign.js","CCGLostSizzlerV142FiveDepthCampaign"],
