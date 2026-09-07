4da946eede0790db631f028ff64aec106377b5eb
joepentony-dot <joepentony@gmail.com>
2026-09-07 17:41:27 +0100

    Bump V10.42 cache after exact Warden source binding

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -5,7 +5,7 @@
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
   const BUILD="V10.42 r3";
-  const CACHE="20260907r15";
+  const CACHE="20260907r16";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-five-depth-campaign.js","CCGLostSizzlerV142FiveDepthCampaign"],
