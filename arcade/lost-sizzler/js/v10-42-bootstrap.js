df6c9864c40fa4f2d508f8b5c4f9ea5cc9800ca8
joepentony-dot <joepentony@gmail.com>
2026-09-08 01:31:19 +0100

    Bust cache for retained Spy inventory repair

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -5,7 +5,7 @@
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
   const BUILD="V10.42 r2";
-  const CACHE="20260907r11";
+  const CACHE="20260908r12";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-five-depth-campaign.js","CCGLostSizzlerV142FiveDepthCampaign"],
