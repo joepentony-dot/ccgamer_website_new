9b34feddcc9b489498dbba1b85b241c3d6dab6bb
joepentony-dot <joepentony@gmail.com>
2026-09-17 22:53:56 +0100

    Align ordered Dungeon bootstrap with r31

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -4,8 +4,8 @@
   if(window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__)return;
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
-  const BUILD="V10.42 r30";
-  const CACHE="20260917r30";
+  const BUILD="V10.42 r31";
+  const CACHE="20260917r31";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-r23-rpg-build-focus.js","CCGLostSizzlerV142R23RpgBuildFocus"],
