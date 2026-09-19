971cefc0b26def7ce08dd8c7aa816e6ba0e080a7
joepentony-dot <joepentony@gmail.com>
2026-09-19 12:58:37 +0100

    Advance Dungeon runtime cache identity to V10.42 r35

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -4,8 +4,8 @@
   if(window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__)return;
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
-  const BUILD="V10.42 r34";
-  const CACHE="20260918r34";
+  const BUILD="V10.42 r35";
+  const CACHE="20260919r35";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-r23-rpg-build-focus.js","CCGLostSizzlerV142R23RpgBuildFocus"],
