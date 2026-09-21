14e0cf9770d9b5c8850de37ab3308717418a2e49
joepentony-dot <joepentony@gmail.com>
2026-09-21 05:07:51 +0100

    Advance Dungeon build to r40

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -4,8 +4,8 @@
   if(window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__)return;
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
-  const BUILD="V10.42 r39";
-  const CACHE="20260921r39";
+  const BUILD="V10.42 r40";
+  const CACHE="20260921r40";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-r23-rpg-build-focus.js","CCGLostSizzlerV142R23RpgBuildFocus"],
