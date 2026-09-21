acb241cf7e64e9c85bad44be97969b981212cf12
joepentony-dot <joepentony@gmail.com>
2026-09-21 02:08:41 +0100

    Advance Dungeon mobile zoom build to r37

--- arcade/lost-sizzler/js/v10-42-bootstrap.js
+++ arcade/lost-sizzler/js/v10-42-bootstrap.js
@@ -4,8 +4,8 @@
   if(window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__)return;
   window.__CCG_LOST_SIZZLER_V142_BOOTSTRAP__=true;
 
-  const BUILD="V10.42 r36";
-  const CACHE="20260921r36a";
+  const BUILD="V10.42 r37";
+  const CACHE="20260921r37";
   const modules=[
     ["v10-42-procedural-overhaul.js","CCGLostSizzlerV142ProceduralOverhaul"],
     ["v10-42-r23-rpg-build-focus.js","CCGLostSizzlerV142R23RpgBuildFocus"],
