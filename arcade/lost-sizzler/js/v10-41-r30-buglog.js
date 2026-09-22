48d7860650a4927661a346defbb470cbf72c0323
joepentony-dot <joepentony@gmail.com>
2026-09-22 11:28:27 +0100

    R51: keep retired buglog source unchanged

--- arcade/lost-sizzler/js/v10-41-r30-buglog.js
+++ arcade/lost-sizzler/js/v10-41-r30-buglog.js
@@ -72,12 +72,7 @@
     }
     return true;
   }
-  const latestBuildOnly=()=>document.getElementById("developer-changelog")?.dataset?.latestBuildOnly==="true";
-  let timer=0;
-  if(!latestBuildOnly()){
-    timer=setInterval(()=>{if(latestBuildOnly()||mount()){clearInterval(timer);timer=0}},80);
-    mount();
-  }
+  let timer=setInterval(()=>{if(mount()){clearInterval(timer);timer=0}},80);mount();
   addEventListener("pagehide",()=>{if(timer)clearInterval(timer)},{once:true});
   window.CCGLostSizzlerV141R30BugLog={entries,mount};
 })();
