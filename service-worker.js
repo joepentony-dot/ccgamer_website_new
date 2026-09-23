437e5854618946b7e663d1e92c9a2470ac2d06a3
joepentony-dot <joepentony@gmail.com>
2026-09-23 05:59:42 +0100

    Teach service worker the Dungeon Carnage public route

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 "use strict";
 
 const CACHE_VERSION = "2026-08-25-public-release-v10";
-const CODE_CACHE_VERSION = "2026-09-22-public-code-v6";
+const CODE_CACHE_VERSION = "2026-09-23-public-code-v7";
 const SHELL_CACHE = `ccg-shell-${CACHE_VERSION}-${CODE_CACHE_VERSION}`;
 const PAGE_CACHE = `ccg-pages-${CACHE_VERSION}`;
 const CODE_CACHE = `ccg-code-${CODE_CACHE_VERSION}`;
@@ -53,6 +53,7 @@ const PRIVATE_PATH_PREFIXES = Object.freeze([
 ]);
 
 const LOST_SIZZLER_PATH_PREFIXES = Object.freeze([
+  "/arcade/c64-dungeon-carnage/",
   "/arcade/lost-sizzler/",
   "/games/ccg-games/cheeky-commodore-quest/"
 ]);
