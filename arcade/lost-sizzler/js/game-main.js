297b44558d1624d5f67c9db31d509f8edafe1007
joepentony-dot <joepentony@gmail.com>
2026-09-24 09:59:09 +0100

    Expose local Dungeon session shell to packaged runtime scripts

--- arcade/lost-sizzler/js/game-main.js
+++ arcade/lost-sizzler/js/game-main.js
@@ -1,5 +1,5 @@
 "use strict";
-net=new window.CCGNetwork.RoomNetwork({onMembers,onPacket});
+net=new window.CCGNetwork.RoomNetwork({onMembers,onPacket});window.net=net;
 
 function installEarlyStableResize(){
   if(window.__CCG_LOST_SIZZLER_EARLY_RESIZE_GUARD__)return;
